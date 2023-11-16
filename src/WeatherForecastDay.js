7922e055a6bb28d1524feec20f4b78983055031e
cassidyct <133056891+cassidyct@users.noreply.github.com>
2023-11-16 18:21:04 -0500

    Added WeatherForecastDay with edits

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -3,17 +3,26 @@ import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
   function maxTemperature() {
-    let temperature = Math.round(props.data.max);
+    let temperature = Math.round(props.data.temp.max);
     return `${temperature}°`;
   }
 
   function minTemperature() {
-    let temperature = Math.round(props.data.min);
+    let temperature = Math.round(props.data.temp.min);
     return `${temperature}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
   }
   return (
     <div>
-      <div className="weather-forecast-date">{props.data.dt}</div>
+      <div className="weather-forecast-date">{day()}</div>
       <WeatherIcon code={props.data.weather[0].icon} size={36} />
       <div className="weather-forecast-temperatures">
         <span className="weather-forecast-temperature-max">
